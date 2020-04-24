588494d597b7072ef0dabaf6587fe4501b527a3d
Samuel-Dufour <9041103+Samuel-Dufour@users.noreply.github.com>
2020-04-24 11:53:54 +0200

    Test entretien

--- ShopApi/Controllers/ProductsController.cs
+++ ShopApi/Controllers/ProductsController.cs
@@ -1,20 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
+
 using ShopApi.Models;
+
+using System;
+using System.Threading.Tasks;
 
 namespace ShopApi.Controllers
 {
-    [ApiController]    
+    [ApiController]
+    [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
-        public ProductsController()
+        private readonly ShopDBContext _dbContext;
+
+        public ProductsController(ShopDBContext dbContext)
         {
-            
-        }        
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<ActionResult> GetAllAsync()
+        {
+            var entities = await _dbContext.Product.ToListAsync();
+            return Ok(entities);
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<ActionResult> GetByIdAsync(int productId)
+        {
+            var product = await _dbContext.Product.FindAsync(productId);
+
+            if (product == null) return NotFound();
+
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Product product)
+        {
+            product.CreationDate = DateTime.Now;
+            _dbContext.Product.Add(product);
+            await _dbContext.SaveChangesAsync();
+
+
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Product product)
+        {
+            var productToUpdate = await _dbContext.Product.FindAsync(product.Id);
+
+            if (product == null) return NotFound();
+
+            productToUpdate.Name = product.Name;
+            productToUpdate.Price = product.Price;
+            productToUpdate.Stock = product.Stock;
+
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(product);
+        }
     }
 }
